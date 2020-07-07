63ac4af556062758af9299f60a432ae766273963
ZoraizQ <21100130@lums.edu.pk>
2020-07-07 19:25:28 +0500

    add: video transparency and sound

--- Assets/Main/Assets/Scripts/MainScene.cs
+++ Assets/Main/Assets/Scripts/MainScene.cs
@@ -19,6 +19,37 @@ namespace AllSamplesLauncher
     {
         public List<Button> Buttons = new List<Button>();
 
+        private static SceneRecorder sceneRecorder;
+
+        private void Awake()
+        {
+            if (sceneRecorder == null)
+            {
+                var go = new GameObject("SceneRecorder");
+                sceneRecorder = go.AddComponent<SceneRecorder>();
+                DontDestroyOnLoad(go);
+                foreach (var item in Buttons)
+                {
+                    if (item.gameObject.name == "SparseSpatialMap")
+                    {
+                        sceneRecorder.Record(item, this);
+                    }
+                }
+            }
+            else
+            {
+                sceneRecorder.Recover(this);
+            }
+
+            foreach (var item in Buttons)
+            {
+                item.onClick.AddListener(() =>
+                {
+                    sceneRecorder.Record(item, this);
+                });
+            }
+        }
+
         public void OpenScene(string sceneName)
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
---
+++ Assets/Main/Assets/Scripts/SceneRecorder.cs
@@ -0,0 +1,58 @@
+﻿//================================================================================================================================
+//
+//  Copyright (c) 2015-2020 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
+//  EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
+//  and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
+//
+//================================================================================================================================
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AllSamplesLauncher
+{
+    public class SceneRecorder : MonoBehaviour
+    {
+        private string buttonName;
+
+        public void Record(Button button, MainScene main)
+        {
+            Button recordButton = null;
+            foreach (var item in main.Buttons)
+            {
+                if (item.gameObject.name == buttonName)
+                {
+                    recordButton = item;
+                }
+            }
+            if (recordButton == button)
+            {
+                return;
+            }
+            else
+            {
+                if (recordButton != null)
+                    recordButton.targetGraphic.color *= 2f;
+            }
+            buttonName = button.gameObject.name;
+            button.targetGraphic.color *= 0.5f;
+        }
+
+        public void Recover(MainScene main)
+        {
+            Button recordButton = null;
+            foreach (var item in main.Buttons)
+            {
+                if (item.gameObject.name == buttonName)
+                {
+                    recordButton = item;
+                }
+            }
+            if (recordButton != null)
+            {
+                recordButton.targetGraphic.color *= 0.5f;
+                recordButton.onClick.Invoke();
+            }
+        }
+    }
+}
