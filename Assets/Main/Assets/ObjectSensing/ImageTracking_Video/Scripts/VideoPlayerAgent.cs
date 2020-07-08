1b41bedf3e0fe3b3371f126afc8489086b2046a5
ZoraizQ <21100130@lums.edu.pk>
2020-07-09 03:42:41 +0500

    add: height toggle

--- Assets/Main/Assets/ObjectSensing/ImageTracking_Video/Scripts/VideoPlayerAgent.cs
+++ Assets/Main/Assets/ObjectSensing/ImageTracking_Video/Scripts/VideoPlayerAgent.cs
@@ -16,9 +16,15 @@ namespace ImageTracking_Video
         private MeshRenderer meshRenderer;
         public UnityEngine.Video.VideoPlayer player;
         public GameObject textDisplay;
+        public GameObject heightDisplay;
+
         public float visibility = 0.5f;
         private int visIndex = 0;
         public float[ ] visibilityOptions = new float[ ]{0.75f, 0.5f, 0.25f, 0.1f, 0.05f} ;
+
+        public float height = 1.0f;
+        private int heightIndex = 0;
+        public float[ ] heightOptions = new float[ ]{1.0f, 1.1f, 1.25f, 1.5f, 1.75f, 2.0f} ;
 
         private bool ready = true;
         private bool prepared;
@@ -84,6 +90,19 @@ namespace ImageTracking_Video
 
             textDisplay.GetComponent<UnityEngine.UI.Text>().text = "Visibility: " + visibility.ToString();
             meshRenderer.material.color = new Color(1.0f,1.0f,1.0f, visibility);
+        }
+
+        public void changeHeight()
+        {
+            if (heightIndex == heightOptions.Length) {
+                heightIndex = 0;
+            }
+
+            height = heightOptions[heightIndex];
+            heightIndex += 1;
+
+            heightDisplay.GetComponent<UnityEngine.UI.Text>().text = "Height: " + height.ToString();
+            gameObject.transform.localScale = new Vector3(5f, 5f*height, 1f);
         }
     }
 }
