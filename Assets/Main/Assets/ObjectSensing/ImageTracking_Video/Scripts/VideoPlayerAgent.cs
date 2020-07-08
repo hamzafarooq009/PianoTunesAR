33facc12ecc001365df7351c282048fca70098cd
ZoraizQ <21100130@lums.edu.pk>
2020-07-08 19:58:06 +0500

    fix: tracker script detached

--- Assets/Main/Assets/ObjectSensing/ImageTracking_Video/Scripts/VideoPlayerAgent.cs
+++ Assets/Main/Assets/ObjectSensing/ImageTracking_Video/Scripts/VideoPlayerAgent.cs
@@ -1,12 +1,4 @@
-﻿//================================================================================================================================
-//
-//  Copyright (c) 2015-2020 VisionStar Information Technology (Shanghai) Co., Ltd. All Rights Reserved.
-//  EasyAR is the registered trademark or trademark of VisionStar Information Technology (Shanghai) Co., Ltd in China
-//  and other countries for the augmented reality technology developed by VisionStar Information Technology (Shanghai) Co., Ltd.
-//
-//================================================================================================================================
-
-using easyar;
+﻿using easyar;
 using System.Collections;
 using System.IO;
 using UnityEngine;
